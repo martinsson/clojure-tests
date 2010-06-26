437d0765cf719eecb28d48c92e72e42d41b7dcf7
johan martinsson <jm1974@johan-martinssons-MacBook-Pro.local>
2010-06-26 15:03:03 +0200

    added parallell version of perfectnumbersInRange

--- src/PerfectNumbersTest.java
+++ src/PerfectNumbersTest.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
 import static java.util.Arrays.asList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 
 import org.junit.Test;
 
@@ -54,9 +63,9 @@ public class PerfectNumbersTest {
 	}
 
         List<Integer> perfectNumbersInRange() {
-            List<Integer> range = range(10);
+            final int ulimit = 10;
             List<Integer> ret = new ArrayList<Integer>();
-            for (Integer number : range) {
+            for (Integer number : range(ulimit)) {
                 if (isPerfect(number)) ret.add(number);
             }
             return ret;
@@ -68,6 +77,38 @@ public class PerfectNumbersTest {
                 range.add(j);
             }
             return range;
+        }
+
+        List<Integer> perfectInParallell() {
+        Integer ulimit = 10;
+        List<Callable<Integer>> calculations = new ArrayList<Callable<Integer>>();
+        for (final Integer number : range(ulimit)) {
+            calculations.add(new Callable<Integer>() {
+                public Integer call() throws Exception {
+                    if (isPerfect(number)) return number;
+                    else return 0;
+                }
+            });
+        }
+        Set<Integer> perfectNumbers = new TreeSet<Integer>();
+        try {
+            ExecutorService executor = Executors.newCachedThreadPool();
+            List<Future<Integer>> results = executor.invokeAll(calculations);
+            for (Future<Integer> future : results) {
+                try {
+                    perfectNumbers.add(future.get());
+                } catch (ExecutionException ex) {
+                    Logger.getLogger(PerfectNumbersTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(PerfectNumbersTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return perfectNumbers;
+
+
+
         }
 	
 }
